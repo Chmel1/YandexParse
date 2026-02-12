249b27faf0e093dbf9e37246f70840fdfbcfa574
Chmel1 <besvlat@bk.ru>
2026-02-12 13:25:21 +0300

    Добавил авторизацию через breeze

---
+++ yandex-reviews/app/Http/Controllers/ReviewController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Review;
+use Illuminate\Http\Request;
+
+class ReviewController extends Controller
+{
+    //Список отзывов
+    public function index(){
+        return Review::orderBy('published_at','desc')->paginate(10);
+    }
+
+    public function storeSettings(Request $request){
+        $request->validate([
+            'url'=>'required|url|contains:yandex.ru'
+        ]);
+
+        cache(['yandex_map_url' =>$request->url], now()->addYears(1));
+        return response()->json(['message'=>'Ссылка сохранена'])ж
+    }
+}
